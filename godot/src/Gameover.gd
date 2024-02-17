0cc5ba63bee517aa41894e09d86fcc3fd14ee29d
myin <minyin142p@gmail.com>
2024-02-17 19:28:17 +0100

    fix: unpause game when pressing try again

--- godot/src/Gameover.gd
+++ godot/src/Gameover.gd
@@ -3,7 +3,10 @@ extends Control
 @export var restart: Button
 
 func _ready():
-	restart.pressed.connect(func(): get_tree().reload_current_scene())
+	restart.pressed.connect(func():
+		get_tree().paused = false
+		get_tree().reload_current_scene()
+	)
 	visibility_changed.connect(func():
 		get_tree().paused = visible
 	)
