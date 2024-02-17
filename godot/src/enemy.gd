6264c4cc7413bfb7d5363e785b570d34c87bba00
myin <minyin142p@gmail.com>
2024-02-17 18:51:54 +0100

    add soft collision

--- godot/src/enemy.gd
+++ godot/src/enemy.gd
@@ -6,10 +6,13 @@ signal died()
 @export var accel := 500
 
 @onready var player := get_tree().get_first_node_in_group("player")
+@onready var soft_collision = $SoftCollision
 
 func _physics_process(delta):
 	var dir = global_position.direction_to(player.global_position)
 	velocity = velocity.move_toward(dir * speed, accel * delta)
+	velocity += soft_collision.get_push_vector() * delta
+	
 	move_and_slide()
 
 
