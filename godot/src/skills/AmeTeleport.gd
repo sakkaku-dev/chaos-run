16cde3d39dee27b0e946bb31682d1ceeb5c8f237
myin <minyin142p@gmail.com>
2024-02-21 21:07:10 +0100

    feat: add tp marker for ame teleport

--- godot/src/skills/AmeTeleport.gd
+++ godot/src/skills/AmeTeleport.gd
@@ -1,11 +1,31 @@
 extends Skill
 
 @export var teleport_distance := 100
+@export var rect: ColorRect
 
-func pressed(player: Player):
-	pass # TODO: show teleport position
+var is_pressed = false
+var player: Player
+
+func _ready():
+	rect.hide()
+
+func pressed(p: Player):
+	is_pressed = true
+	player = p
 
 func released(player: Player):
+	player.global_position += _get_dir()
+	is_pressed = false
+
+func _get_dir():
 	var motion = player.get_motion()
 	var dir = motion if motion else player.global_position.direction_to(player.get_global_mouse_position())
-	player.global_position += dir * teleport_distance
+	return dir * teleport_distance
+
+func _process(delta):
+	if not is_pressed:
+		rect.hide()
+		return
+	
+	rect.show()
+	rect.global_position = player.global_position + _get_dir()
