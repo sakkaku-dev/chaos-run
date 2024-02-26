91127b46a66413e996e461407c82cc82035d2099
myin <minyin142p@gmail.com>
2024-02-26 23:13:49 +0100

    feat: add dice impact sound

--- godot/src/skills/dice_attack.gd
+++ godot/src/skills/dice_attack.gd
@@ -1,13 +1,28 @@
 extends Node2D
 
 var time := 0.0
+var hit := false
+
+@onready var impact = $Impact
+@onready var collision_shape_2d = $HitBox/CollisionShape2D
 
 func _process(delta):
 	time += delta
+
+	if not hit:
+		hit = true
+		impact.play()
 	
 	if time >= 0.2:
-		queue_free()
-
+		_free()
 
 func _on_hit_box_hit():
+	impact.play()
+	_free()
+	
+func _free():
+	if impact.playing:
+		collision_shape_2d.set_deferred("disabled", true)
+		hide()
+		await impact.finished
 	queue_free()
